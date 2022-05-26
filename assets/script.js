367853d20c3dea67f6d8788b04f3418dc54726ad
willihane <williane.pereira@outlook.com>
2022-05-25 23:12:31 -0300

    Landing page - versão 2.0

--- assets/script.js
+++ assets/script.js
@@ -23,37 +23,38 @@ function validaEmail() {
     emailOk = false
             }
 }
- 
- function validaAssunto() {
-     let txtAssunto = document.querySelector("#txtAssunto")
- 
-     if (assunto.value.length >= 500) {
-         txtAssunto.innerHTML = "Digite no máximo 500 caracteres."
-         txtAssunto.style.color = "Red" 
-         txtAssunto.style.display = "block"
-         assuntoOk = false
-     
-     } else {
-         txtAssunto.style.display = "none"
-         assuntoOk = true
-     }
- 
- }
- 
- function enviar() {
-     if(emailOk == true && assuntoOk == true) {
-         alert ("Formulário enviado com sucesso!")
-     } else {
-         alert ("Por favor, preencha o formulário corretamente antes de enviar!")
-     }
- }
- 
- function mapaZoom() {
-     mapa.style.width = "800px"
-     mapa.style.height = "600px"
- }
- 
- function mapaNormal () {
-     mapa.style.width = "400"
-     mapa.style.height = "250"
- }
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector("#txtAssunto")
+
+    if (assunto.value.length >= 500) {
+        txtAssunto.innerHTML = "Digite no máximo 500 caracteres."
+        txtAssunto.style.color = "Red" 
+        txtAssunto.style.display = "block"
+        assuntoOk = false
+    
+    } else {
+        txtAssunto.style.display = "none"
+        assuntoOk = true
+    }
+
+}
+
+function enviar() {
+    if(emailOk == true && assuntoOk == true) {
+        alert ("Formulário enviado com sucesso!")
+    } else {
+        alert ("Por favor, preencha o formulário corretamente antes de enviar!")
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = "800px"
+    mapa.style.height = "600px"
+}
+
+function mapaNormal () {
+    mapa.style.width = "400px"
+    mapa.style.height = "250px"
+}
+
